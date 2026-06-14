b8862d20c6d50d2fa6d0c48fc5d30d0c401e2b01
Lazar Slavković-Raco <slavkovic.lazar96@gmail.com>
2026-06-14 22:20:33 +0200

    add: tables for star generation

---
+++ data/tables.py
@@ -0,0 +1,82 @@
+STAR_TYPE_DETERMINATION_TYPE = {
+    2: "Special",
+    3: "M",
+    4: "M",
+    5: "M",
+    6: "M",
+    7: "K",
+    8: "K",
+    9: "G",
+    10: "G",
+    11: "F",
+    12: "Hot",
+}
+STAR_TYPE_DETERMINATION_HOT = {
+    2: "A",
+    3: "A",
+    4: "A",
+    5: "A",
+    6: "A",
+    7: "A",
+    8: "A",
+    9: "A",
+    10: "B",
+    11: "B",
+    12: "O",
+}
+
+STAR_TYPE_DETERMINATION_SPECIAL = {
+    2: "Class VI",
+    3: "Class VI",
+    4: "Class VI",
+    5: "Class VI",
+    6: "Class IV",
+    7: "Class IV",
+    8: "Class IV",
+    9: "Class III",
+    10: "Class III",
+    11: "Giants",
+    12: "Giants",
+}
+
+STAR_TYPE_DETERMINATION_UNUSUAL = {
+    2: "Peculiar",
+    3: "Class VI",
+    4: "Class IV",
+    5: "BD",
+    6: "BD",
+    7: "BD",
+    8: "D",
+    9: "D",
+    10: "D",
+    11: "Class III",
+    12: "Giants",
+}
+
+STAR_TYPE_DETERMINATION_GIANTS = {
+    2: "Class III",
+    3: "Class III",
+    4: "Class III",
+    5: "Class III",
+    6: "Class III",
+    7: "Class III",
+    8: "Class III",
+    9: "Class II",
+    10: "Class II",
+    11: "Class Ib",
+    12: "Class Ia",
+}
+
+STAR_TYPE_DETERMINATION_PECULIAR = {
+    2: "Black Hole",
+    3: "Pulsar",
+    4: "Neutron Star",
+    5: "Nebula",
+    6: "Nebula",
+    7: "Protostar",
+    8: "Protostar",
+    9: "Protostar",
+    10: "Star Cluster",
+    11: "Anomaly",
+    12: "Anomaly",
+}
